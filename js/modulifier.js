2cba0010022eb853c3030bb0ddd6a53be7a9b146
Thomas J Bradley <hey@thomasjbradley.ca>
2016-09-28 13:44:47 -0400

    Change formatting to match my current style

--- js/modulifier.js
+++ js/modulifier.js
@@ -1,45 +1,43 @@
 (function ($, $$) {
-  "use strict";
+  'use strict';
 
-  var
-    hash = window.location.hash.replace(/#/, ''),
-    hashBits,
+  var hash = window.location.hash.replace(/#/, '');
+  var hashBits;
 
-    $controls = $('#controls'),
-    $allChecks = $controls.querySelectorAll('input[type="checkbox"]'),
-    $output = $('#output'),
+  var $controls = $('#controls');
+  var $allChecks = $controls.querySelectorAll('input[type="checkbox"]');
+  var $output = $('#output');
 
-    view = function view (name, vals) {
-      return prepareTemplate(name, vals);
-    },
+  var view = function view (name, vals) {
+    return prepareTemplate(name, vals);
+  };
 
-    getCheckedModules = function () {
-      return $controls.querySelectorAll('input[type="checkbox"]:checked');
-    },
+  var getCheckedModules = function () {
+    return $controls.querySelectorAll('input[type="checkbox"]:checked');
+  };
 
-    findModuleCode = function (moduleChecks) {
-      var output = [], hash = [];
+  var findModuleCode = function (moduleChecks) {
+    var output = [], hash = [];
 
-      Array.prototype.forEach.call(moduleChecks, function (elem) {
-        output.push(view(elem.value).trim());
-        hash.push(elem.value.replace('css-', ''));
-      });
+    Array.prototype.forEach.call(moduleChecks, function (elem) {
+      output.push(view(elem.value).trim());
+      hash.push(elem.value.replace('css-', ''));
+    });
 
-      return {
-        css: output,
-        hash: hash
-      };
-    },
+    return {
+      css: output,
+      hash: hash
+    };
+  };
 
-    buildOutput = function (output) {
-      var build = window.location.protocol + '//' + window.location.host + window.location.pathname + '#' + output.hash.join(';');
+  var buildOutput = function (output) {
+    var build = window.location.protocol + '//' + window.location.host + window.location.pathname + '#' + output.hash.join(';');
 
-      output.css.unshift(view('css-base', { build: build }).trim());
-      $output.innerHTML = output.css.join('\n\n');
+    output.css.unshift(view('css-base', { build: build }).trim());
+    $output.innerHTML = output.css.join('\n\n');
 
-      window.location.hash = output.hash.join(';');
-    }
-  ;
+    window.location.hash = output.hash.join(';');
+  };
 
   'keyup change submit'.split(' ').forEach(function (singleEvent) {
     $controls.addEventListener(singleEvent, function (e) {
